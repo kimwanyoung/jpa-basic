88e45009dfcea72424179893a53cc0d06b7b0fed
kimwanyoung <dhks2869@gmail.com>
2024-02-08 19:19:02 +0900

    feat : Jpa, Jpql 실습

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,16 +1,34 @@
 package hellojpa;
 
 import jakarta.persistence.*;
+import java.util.List;
 
 public class JpaMain {
 
     public static void main(String[] args) {
-
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        EntityManager em = emf.createEntityManager();
-        //code
 
-        em.close();
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+        try {
+            List<Member> findMembers = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(5).setMaxResults(8)
+                    .getResultList();
+
+            for (Member findMember : findMembers) {
+                System.out.println("findMember = " + findMember.getName());
+            }
+
+            tx.commit();
+        } catch (Exception exception) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
         emf.close();
+
     }
 }
